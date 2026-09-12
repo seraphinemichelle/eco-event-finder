077dc2fc506da08ea9f1a95d6a2d4f44598a1fa1
seraphinemichelle <seraphinemichellee@gmail.com>
2026-09-12 16:34:22 +0700

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'g-dark':   '#1B3A2D',
+        'g-main':   '#2D6A4F',
+        'g-lime':   '#74C69D',
+        'g-bg':     '#F0F4F1',
+        'g-wire':   '#C8D8CE',
+        'g-gray':   '#8A9E93',
+        'g-accent': '#D4E6DA',
+        'g-hi':     '#B7E4C7',
+        'g-yellow': '#E9C46A',
+        'g-red':    '#E07070',
+      },
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+        mono:   ['Space Mono', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
